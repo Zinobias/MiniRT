5f5a2a8eec93462dbd65c55d7addc45544ffc527
Zinobias <66080544+Zinobias@users.noreply.github.com>
2020-06-22 13:17:14 +0200

    Automated git

--- miniRT/miniRT.c
+++ miniRT/miniRT.c
@@ -171,7 +171,6 @@ void	rendering(void)
 	mlx.img = mlx_new_image(mlx.mlx, 1920, 1080);
 	if (!mlx.mlx || !mlx.img)
 		error(MLX);
-	// 				mlx_ptr, x, y, title
 	mlx.win = mlx_new_window(mlx.mlx, 1920, 1080, "hello worlddd");
 	if (!mlx.mlx || !mlx.win)
 		error(MLX);
@@ -182,28 +181,10 @@ void	rendering(void)
 			my_mlx_pixel_put(&mlx, i, j, rgba(0, 55, 55, 255));
 		}
 	}
- 	// my_mlx_pixel_put(&mlx, 5, 5, 0x00FF0000);
 	mlx_put_image_to_window(&mlx, mlx.win, mlx.img, 0, 0);
 	mlx_key_hook(mlx.win, close_window_esc, &mlx);
 	mlx_hook(mlx.win, 17, 0, close_win_x, &mlx);
 	mlx_loop(mlx.mlx);
-	// escape = 53
-	// int	mlx_pixel_put(void *mlx_ptr, void *win_ptr, int x, int y, int color);
-	// /*
-	// **  origin for x & y is top left corner of the window
-	// **  y down is positive
-	// **  color is 0x AA RR GG BB format
-	// **  x and y must fit into the size of the window, no control is done on the values
-	// */
-	// char	*mlx_get_data_addr(void *img_ptr, int *bits_per_pixel,
-	// 		   int *size_line, int *endian);
-	// /*
-	// **  endian : 0 = graphical sever is little endian, 1 = big endian
-	// **  usefull in a network environment where graphical app show on a remote monitor that can have a different endian
-	// */
-	// int	mlx_put_image_to_window(void *mlx_ptr, void *win_ptr, void *img_ptr,
-	// 			int x, int y);
-	// unsigned int	mlx_get_color_value(void *mlx_ptr, int color);
 	return ;
 }
 
